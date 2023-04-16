1bb87e0a9692050311d65aa062870896306b49b7
DonAlPatino <valp@inbox.ru>
2023-04-16 18:36:15 +0300

    feat: encode-line

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    let myMap = new Map();
+    for (let fullname of domains) {
+        let tochka = 0;
+        console.log(fullname)
+        while (fullname && tochka>-1) {
+            if (myMap.has(fullname)) {
+                myMap.set(fullname, myMap.get(fullname) + 1);
+            } else {
+                myMap.set(fullname, 1);
+            }
+            tochka = fullname.indexOf('.')
+            fullname = fullname.slice(tochka + 1)
+        }
+    }
+    console.log(myMap)
+    return myMap
 }
 
 module.exports = {
-  getDNSStats
+    getDNSStats
 };
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {//
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let newstr = "";
+  for (let i=0; i < str.length;i++){
+    let p = 1;
+    while(str[i]===str[i+1]){
+      p++
+      i++
+    }
+    if (p>1) newstr = newstr + p + str[i]
+    else newstr = newstr + str[i]
+  }
+  return newstr
 }
 
 module.exports = {
